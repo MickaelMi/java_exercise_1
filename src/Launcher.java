b73785262146c1d20346871ca92e8fe3ad9b029d
mickael.michel <mickael@Mickael.Michel>
2022-02-09 17:11:00 +0100

    Partie 3

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,12 +4,14 @@ public class Launcher {
     public static void main(String[] args) {
         System.out.println("Mot de bienvenue !");
         Scanner scan = new Scanner(System.in);
-        String str = scan.nextLine();
-        if (str.equals("quit")) {
-            System.out.println("Command " + str);
-        }
-        else {
-            System.out.println("Unknown Command");
+        while (true) {
+            String str = scan.nextLine();
+            if (str.equals("quit")) {
+                System.out.println("Command " + str);
+                break;
+            } else {
+                System.out.println("Unknown Command");
+            }
         }
     }
 }
